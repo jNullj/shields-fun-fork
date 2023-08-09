beb9faac2399314ad1ab55dcaa5d7e0f0759a060
jNullj <15849761+jNullj@users.noreply.github.com>
2023-08-09 23:44:10 +0300

    fix tags

--- .github/actions/docusaurus-swizzled-warning/index.js
+++ .github/actions/docusaurus-swizzled-warning/index.js
@@ -65,8 +65,8 @@ async function run() {
             client,
             'cloud-annotations',
             'docusaurus-openapi',
-            oldVersion,
-            newVersion,
+            `v${oldVersion}`,
+            `v${newVersion}`,
           )
           const changedComponents = overideComponents.filter(
             componenet =>
